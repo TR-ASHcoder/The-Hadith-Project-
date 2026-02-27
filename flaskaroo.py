8b5b6b94ac357465257c4d39e2a15661e095db9c
TR-ASHcoder <aymanraf7@gmail.com>
2026-02-27 19:09:20 +1100

    fixed old api
    made it so only text that can fit on the screen can be viewed
    
    gave it gunicorn support

--- flaskaroo.py
+++ flaskaroo.py
@@ -1,24 +1,37 @@
 from flask import Flask, render_template
 import requests
-import json
-
+import random
 
 app = Flask(__name__)
 
 def get_hadith():
-  url = "https://random-hadith-generator.vercel.app/bukhari/?data="
-  response = json.loads(requests.request("GET", url).text) 
-  hadith = response['data']['hadith_english']
-  fact = response['data']['refno']
-  narr = response['data']['header']
-  chap = response['data']['chapterName']
-  return hadith, fact, narr, chap
-  
+    page = random.randint(1, 757)
+    url = f"https://hadithapi.pages.dev/api/bukhari?page={page}"
+    response = requests.get(url).json()
+    
+
+    filtered = [h for h in response['results'] if 100 <= len(h['hadith_english']) <= 500]
+    
+ 
+    if not filtered:
+        return get_hadith()
+    
+    item = random.choice(filtered)
+    
+    hadith = item['hadith_english']
+    fact = item['refno']
+    narr = item['header']
+    chap = item['chapterName']
+    
+    return hadith, fact, narr, chap
+
 @app.route('/')
 def index():
     hadith, fact, narr, chap = get_hadith()
-    return render_template("index.html", hadith = hadith, fact = fact, narr = narr, chap = chap)
- 
-app.run(host="0.0.0.0", port=8080)
+    return render_template("index.html", hadith=hadith, fact=fact, narr=narr, chap=chap)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080)
+
 
 # made by TR ASH
